86d9c07264a47b49af307f1e7f77edc92bc31440
Roman <roman.baytala@gmail.com>
2023-03-10 17:59:46 +0200

    123

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,8 @@
-const getAll = require('./contacts/getAll');
-const getById = require('./contacts/getById');
-const add = require('./contacts/add');
-const removeById = require('./contacts/removeById');
-const updateById = require('./contacts/updateById');
+const { getAll } = require('./contacts/getAll');
+const { getById } = require('./contacts/getById');
+const { add } = require('./contacts/add');
+const { removeById } = require('./contacts/removeById');
+const { updateById } = require('./contacts/updateById');
 
 const ctrlWrapper = require('../helpers/ctrlWrapper');
 
